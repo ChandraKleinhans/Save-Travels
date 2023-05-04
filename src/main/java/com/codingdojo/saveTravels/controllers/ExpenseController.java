39ff9411773f3998a1270630e7d10610cef7a717
Chandra <chandra.kleinhans@gmail.com>
2023-05-04 11:52:55 -0500

    initial commit

---
+++ src/main/java/com/codingdojo/saveTravels/controllers/ExpenseController.java
@@ -0,0 +1,78 @@
+package com.codingdojo.saveTravels.controllers;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import com.codingdojo.saveTravels.models.Expense;
+import com.codingdojo.saveTravels.services.ExpenseService;
+
+
+@Controller
+public class ExpenseController {
+	@Autowired
+	private ExpenseService expenseService;
+
+
+	@GetMapping("/expenses")
+	public String expenses(@ModelAttribute("expense") Expense expense, Model model) {
+		List<Expense> expenses = expenseService.allExpenses();
+		model.addAttribute("expenses", expenses);
+		return "HomeWithForm.jsp";
+	}
+	
+	@GetMapping("/expenses/{id}")
+	public String expenses(@PathVariable("id") Long id,Model model) {
+		Expense expense = expenseService.findExpense(id);
+		model.addAttribute("expense", expense);
+		return "oneExpense.jsp";
+	}
+	
+	@PostMapping("/expenses")
+	public String create(@Valid @ModelAttribute("expense") Expense expense, BindingResult result,Model model) {
+		if(result.hasErrors()) {
+			List<Expense> expenses = expenseService.allExpenses();
+			model.addAttribute("expenses",expenses);
+			return"HomeWithForm.jsp";
+		}else {
+			expenseService.createExpense(expense);
+			return "redirect:/expenses";
+		}
+	}
+	
+	@GetMapping("/expenses/edit/{id}")
+	public String edit(@PathVariable("id") Long id,Model model) {
+		Expense expense = expenseService.findExpense(id);
+		model.addAttribute("expense",expense);
+		return "edit.jsp";
+	}
+	
+	
+	@PutMapping("/expenses/edit/{id}")
+	public String update(@Valid @ModelAttribute("expense") Expense expense,BindingResult result) {
+		if(result.hasErrors()) {
+			return "edit.jsp";
+		}else {
+			expenseService.updateExpense(expense);
+			return "redirect:/expenses";
+		}
+	}
+	
+	@DeleteMapping("/expenses/delete/{id}")
+	public String destroy(@PathVariable("id") Long id) {
+		expenseService.deleteExpense(id);
+		return "redirect:/expenses";
+	}
+	
+}
